9f312176ba07649a54095ae0279c49e501d36ffe
Honório Gomes <hgunicamp@gmail.com>
2023-12-18 11:35:56 -0300

    Inserting macros to print the backgrounds.

--- projects/background/main.c
+++ projects/background/main.c
@@ -1,17 +1,16 @@
 #include <genesis.h>
 #include <resources.h>
+#include "inc/background.h"
 
 int main() {
     u16 tile_index = TILE_USER_INDEX;
 
     // Inserting background on screen.
-    PAL_setPalette(PAL0, bg1.palette->data, DMA);
-    VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, tile_index), 0, 0, FALSE, DMA);
+    PRINT_BG(BG_B, PAL0, bg1, tile_index);
 
     // Inserting foreground on screen.
     tile_index += bg1.tileset->numTile;
-    PAL_setPalette(PAL1, fg1.palette->data, DMA);
-    VDP_drawImageEx(BG_A, &fg1, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, tile_index), 0, 0, FALSE, DMA);
+    PRINT_BG(BG_A, PAL1, fg1, tile_index);
 
     // Main Loop.
     while(1) {
