31f7ec90c622eb5c3a6e0a1fc47446653effe8b5
Honório Gomes <hgunicamp@gmail.com>
2023-12-18 23:48:04 -0300

    Inserting constants to define the layer speed. Removing magic numbers.

--- projects/parallax/main.c
+++ projects/parallax/main.c
@@ -3,6 +3,10 @@
 #include "inc/background.h"
 #include "inc/parallax.h"
 
+// Parallax relative speed.
+#define LAYER_A_SPEED -2
+#define LAYER_B_SPEED -1
+
 int main() {
     u16 tile_index = TILE_USER_INDEX;
     u16 bg_a_offset = 0, bg_b_offset = 0;
@@ -19,8 +23,8 @@ int main() {
     // Main Loop.
     while(1) {
         // Paralax effect.
-        PARALAX_HORIZONTAL_PLANE_SPEED(BG_B, bg_b_offset, -1);
-        PARALAX_HORIZONTAL_PLANE_SPEED(BG_A, bg_a_offset, -2);
+        PARALAX_HORIZONTAL_PLANE_SPEED(BG_B, bg_b_offset, LAYER_B_SPEED);
+        PARALAX_HORIZONTAL_PLANE_SPEED(BG_A, bg_a_offset, LAYER_A_SPEED);
 
         // End of the frame.
         SYS_doVBlankProcess();
