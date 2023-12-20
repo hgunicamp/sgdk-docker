cc48f19963c62d291e064b3beab480f106721f07
Honório Gomes <hgunicamp@gmail.com>
2023-12-20 18:25:13 -0300

    Replacing the Sprite reference by a structure to group all sprite state information.

--- projects/show_sprite/inc/sprite.h
+++ projects/show_sprite/inc/sprite.h
@@ -1,13 +1,22 @@
 #ifndef _SPRITE_H_
 #define _SPRITE_H_
 
+typedef struct {
+    Sprite *sprite;
+    s16 x_pos;
+    s16 y_pos;
+    u16 sprite_attributes;
+} sprite_struct;
+
 #define INIT_SPRITE(palette_index, resource) do { \
     SPR_init(); \
     PAL_setPalette(palette_index, resource.palette->data, DMA); \
 } while(0)
 
-#define ADD_SPRITE_ON(palette_index, sprite_index, resource, x_pos, y_pos) do { \
-    sprite_index = SPR_addSprite(&resource, x_pos, y_pos, TILE_ATTR(palette_index, FALSE, FALSE, FALSE)); \
+#define ADD_SPRITE(resource, sprite_index) do { \
+    sprite_index.sprite = SPR_addSprite( \
+        &resource, sprite_index.x_pos, sprite_index.y_pos, sprite_index.sprite_attributes \
+    ); \
 } while(0)
 
 #endif
