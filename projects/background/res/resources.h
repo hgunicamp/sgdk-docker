26611a2a7a1e23f3a1926f6e7f064ebf3951a1c2
Honório Gomes <hgunicamp@gmail.com>
2023-12-20 11:17:47 -0300

    Removing the background folder from projects.

--- projects/background/res/resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef _RES_RESOURCES_H_
-#define _RES_RESOURCES_H_
-
-extern const Image bg1;
-extern const Image fg1;
-
-#endif // _RES_RESOURCES_H_
--- projects/background/res/resources.res
+++
@@ -1,2 +0,0 @@
-IMAGE  bg1 "backgrounds/background1.png" BEST 
-IMAGE  fg1 "backgrounds/foreground1.png" BEST 
