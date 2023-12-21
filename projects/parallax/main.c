8389ed6c5011fb4ec5c5953cce8396203762fa5e
Honório Gomes <hgunicamp@gmail.com>
2023-12-21 16:27:04 -0300

    Refactoring the background function to enhance the abstraction.

--- projects/parallax/main.c
+++ projects/parallax/main.c
@@ -9,13 +9,15 @@
 
 int main() {
     u16 tile_index = TILE_USER_INDEX;
-    u16 bg_a_offset = 0, bg_b_offset = 0;
+    background_struct background, foreground;
+
+    // Init the backgrounds layers.
+    background_init(&background, BG_B, PAL0, &bg1);
+    background_init(&foreground,  BG_A, PAL1, &fg1);
 
     // Inserting background on screen.
-    PRINT_BG(BG_B, PAL0, bg1, tile_index);
-
-    // Inserting foreground on screen.
-    PRINT_BG(BG_A, PAL1, fg1, tile_index);
+    PRINT_BG(background, tile_index);
+    PRINT_BG(foreground, tile_index);
 
     // Setting up scrooling configuration.
     VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
@@ -23,8 +25,8 @@ int main() {
     // Main Loop.
     while(1) {
         // Paralax effect.
-        PARALAX_HORIZONTAL_PLANE_SPEED(BG_B, bg_b_offset, LAYER_B_SPEED);
-        PARALAX_HORIZONTAL_PLANE_SPEED(BG_A, bg_a_offset, LAYER_A_SPEED);
+        PARALAX_HORIZONTAL_PLANE_SPEED(background, LAYER_B_SPEED);
+        PARALAX_HORIZONTAL_PLANE_SPEED(foreground, LAYER_A_SPEED);
 
         // End of the frame.
         SYS_doVBlankProcess();
