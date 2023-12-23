3f5c4964e984d323fb1c822d82f613257476c130
Honório Gomes <hgunicamp@gmail.com>
2023-12-23 18:54:29 -0300

    Initial version of the "animate sprite" project.

---
+++ projects/animate_sprite/inc/sprite.h
@@ -0,0 +1,32 @@
+#ifndef _SPRITE_H_
+#define _SPRITE_H_
+
+#define  SPRITE_NO_ANIMATE -1
+
+#include <dma.h>
+#include <sprite_eng.h>
+
+typedef struct {
+    Sprite *sprite;
+    s16 x_pos;
+    s16 y_pos;
+    s16 idle_animation_index;
+    s16 current_animation_index;
+    u16 sprite_attributes;
+} sprite_struct;
+
+#define INIT_SPRITE_PALETTE(palette_index, resource) do { \
+    PAL_setPalette(palette_index, resource.palette->data, DMA); \
+} while(0)
+
+#define ADD_SPRITE(resource, sprite_index, idle_animation) do { \
+    sprite_index.sprite = SPR_addSprite( \
+        &resource, sprite_index.x_pos, sprite_index.y_pos, sprite_index.sprite_attributes \
+    ); \
+    sprite_index.idle_animation_index = sprite_index.current_animation_index = idle_animation; \
+    if (SPRITE_NO_ANIMATE != idle_animation) { \
+        SPR_setAnim(sprite_index.sprite, idle_animation); \
+     } \
+} while(0)
+
+#endif
---
+++ projects/animate_sprite/main.c
@@ -0,0 +1,36 @@
+#include <genesis.h>
+#include "res/resources.h"
+#include "inc/background.h"
+#include "inc/sprite.h"
+
+int main() {
+    u16 tile_index = TILE_USER_INDEX;
+    background_struct background, foreground;
+    sprite_struct axel;
+
+    // Init the backgrounds layers.
+    background_init(&background, BG_B, PAL0, &bg1);
+    background_init(&foreground,  BG_A, PAL1, &fg1);
+
+    // Initialising the sprite struct.
+    axel.x_pos = 25;
+    axel.y_pos = 50;
+    axel.sprite_attributes = TILE_ATTR(PAL2, FALSE, FALSE, TRUE);
+
+    // Inserting background on screen.
+    PRINT_BG(background, tile_index);
+    PRINT_BG(foreground, tile_index);
+
+    // Sprite set up.
+    SPR_init();
+    INIT_SPRITE_PALETTE(PAL2, axel_sprite);
+    ADD_SPRITE(axel_sprite, axel, 1);
+
+    // Main Loop.
+    while(1) {
+        SPR_update();
+        // End of the frame.
+        SYS_doVBlankProcess();
+    }
+    return 0;
+}
