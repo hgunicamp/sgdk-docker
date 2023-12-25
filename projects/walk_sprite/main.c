8cb76c22eea6c609526b941e52e248a2aa13dccb
Honório Gomes <hgunicamp@gmail.com>
2023-12-25 14:06:25 -0300

    Intial axel's actions. - Refactoring the sprites macros to use poiters instead of pure structs. - Installing the axel sprite.

--- projects/walk_sprite/main.c
+++ projects/walk_sprite/main.c
@@ -1,7 +1,7 @@
 #include <genesis.h>
 #include "res/resources.h"
 #include "inc/background.h"
-#include "inc/sprite.h"
+#include "inc/axel_actions.h"
 
 int main() {
     u16 tile_index = TILE_USER_INDEX;
@@ -12,19 +12,13 @@ int main() {
     background_init(&background, BG_B, PAL0, &bg1);
     background_init(&foreground,  BG_A, PAL1, &fg1);
 
-    // Initialising the sprite struct.
-    axel.x_pos = 25;
-    axel.y_pos = 50;
-    axel.sprite_attributes = TILE_ATTR(PAL2, FALSE, FALSE, TRUE);
-
     // Inserting background on screen.
     PRINT_BG(background, tile_index);
     PRINT_BG(foreground, tile_index);
 
     // Sprite set up.
     SPR_init();
-    INIT_SPRITE_PALETTE(PAL2, axel_sprite);
-    INSTALL_SPRITE(axel_sprite, axel, 1);
+    install_axel_sprite(&axel, &axel_sprite);
 
     // Main Loop.
     while(1) {
