e78b21b8cf72865e6d1d4c6db702fce00045e477
Honório Gomes <hgunicamp@gmail.com>
2023-12-26 13:40:29 -0300

    Refactoring the emulator to allow the next development step.

--- projects/action_sprite/inc/joystick_mediator.h
+++ projects/action_sprite/inc/joystick_mediator.h
@@ -5,12 +5,18 @@
 #include <sprite_eng.h>
 #include <joy.h>
 
-typedef struct {
+#define JOY_NO_ACTION_TAKEN 0
+#define JOY_ACTION_TAKEN 1
+
+static u16 joy_last_event[JOY_NUM] = { 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
+
+typedef struct joy_mediator {
     sprite_struct *sprite_index;
-    u16 (*up_action)(sprite_struct *sprite);
-    u16 (*down_action)(sprite_struct *sprite);
-    u16 (*rigth_action)(sprite_struct *sprite);
-    u16 (*left_action)(sprite_struct *sprite);
+    s16 lock_frames;
+    u16 (*up_action)(struct joy_mediator *mediator);
+    u16 (*down_action)(struct joy_mediator *mediator);
+    u16 (*rigth_action)(struct joy_mediator *mediator);
+    u16 (*left_action)(struct joy_mediator *mediator);
 } joystick_mediator_struct;
 
 #define JOY_TRIGGER_IDLE_ACTION(mediator, action_alread_taken) do { \
@@ -26,15 +32,27 @@ typedef struct {
 #define JOY_TRIGGER_X_AXIS_ACTION(mediator, joy_value, action_alread_taken) do { \
     if (!(joy_value & (BUTTON_RIGHT | BUTTON_LEFT))) break; \
     action_alread_taken |= (joy_value & BUTTON_RIGHT) ? \
-        mediator.rigth_action(mediator.sprite_index)  :  \
-        mediator.left_action(mediator.sprite_index); \
+        mediator.rigth_action(&mediator)  :  \
+        mediator.left_action(&mediator); \
 } while(0)
 
 #define JOY_TRIGGER_Y_AXIS_ACTION(mediator, joy_value, action_alread_taken) do { \
     if (!(joy_value & (BUTTON_DOWN | BUTTON_UP))) break; \
     action_alread_taken |= (joy_value & BUTTON_DOWN) ? \
-        mediator.down_action(mediator.sprite_index)  :  \
-        mediator.up_action(mediator.sprite_index); \
+        mediator.down_action(&mediator)  :  \
+        mediator.up_action(&mediator); \
 } while(0)
+
+#define JOY_UNLOCK_MEDIATOR(ptr_mediator)  ptr_mediator->lock_frames = 0
+#define JOY_MEDIATOR_IS_LOCKED(ptr_mediator) ptr_mediator->lock_frames > 0
+#define JOY_GET_CURRENT_STATE(joy) joy_last_event[joy]
+
+u16 joystick_mediator_no_button_action(joystick_mediator_struct *mediator);
+
+u16 joystick_mediator_get_last_joy_event(u16 joy);
+
+void joystick_mediator_reduce_locking_time(joystick_mediator_struct *mediator);
+
+void joystck_event_listener(u16 joy, u16 changed, u16 state);
 
 #endif
---
+++ projects/action_sprite/src/joystick_mediator.c
@@ -0,0 +1,15 @@
+#include "../inc/joystick_mediator.h"
+
+u16 joystick_mediator_no_button_action(joystick_mediator_struct *mediator) {
+    return JOY_NO_ACTION_TAKEN;
+}
+
+void joystick_mediator_reduce_locking_time(joystick_mediator_struct *mediator) {
+    if (!(mediator->lock_frames)) return;
+    mediator->lock_frames--;
+}
+
+void joystck_event_listener(u16 joy, u16 changed, u16 state) {
+    if (!changed) return;
+    joy_last_event[joy] = state;
+}
