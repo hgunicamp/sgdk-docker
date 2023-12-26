6f57be06678d9bf20f78d46e33a975e5e5622422
Honório Gomes <hgunicamp@gmail.com>
2023-12-26 15:49:16 -0300

    Developing the "ATACK ACTION".

--- projects/action_sprite/src/axel_actions.c
+++ projects/action_sprite/src/axel_actions.c
@@ -7,7 +7,11 @@
 
 #define AXEL_IDLE_ANIMATION 1
 #define AXEL_WALKING_ANIMATION 2
-#define AXEL_ACTACK_ANIMATION 3
+#define AXEL_ATACK_ANIMATION 3
+#define AXEL_ATACK_ANIMATION_FRAMES 60
+#define AXEL_NONE_ACTION 0x0000
+#define AXEL_WALK_ACTION 0x0001
+#define AXEL_ATACK_ACTION 0x0002
 #define AXEL_INIT_X_POS 25
 #define AXEL_INIT_Y_POS 50
 #define AXEL_UP_WALKING_LIMT 0
@@ -26,32 +30,38 @@ joystick_mediator_struct axel_joy_mediator;
 
 u16 axel_up_action(joystick_mediator_struct *mediator) {
     sprite_struct *axel = mediator->sprite_index;
-    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_UP(axel->y_pos)) return 0;
+    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_UP(axel->y_pos)) return AXEL_NONE_ACTION;
     axel->y_pos -= AXEL_Y_SPEED;
-    return 1;
+    return AXEL_WALK_ACTION;
 }
 
 u16 axel_down_action(joystick_mediator_struct *mediator) {
     sprite_struct *axel = mediator->sprite_index;
-    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_DOWN(axel->y_pos)) return 0;
+    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_DOWN(axel->y_pos)) return AXEL_NONE_ACTION;
     axel->y_pos += AXEL_Y_SPEED;
-    return 1;
+    return AXEL_WALK_ACTION;
 }
 
 u16 axel_left_action(joystick_mediator_struct *mediator) {
     sprite_struct *axel = mediator->sprite_index;
-    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_LEFT(axel->x_pos)) return 0;
+    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_LEFT(axel->x_pos)) return AXEL_NONE_ACTION;
     axel->x_pos -= AXEL_X_SPEED;
     SPR_setHFlip(axel->sprite, FALSE);
-    return 1;
+    return AXEL_WALK_ACTION;
 }
 
 u16 axel_right_action(joystick_mediator_struct *mediator) {
     sprite_struct *axel = mediator->sprite_index;
-    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_RIGHT(axel->x_pos)) return 0;
+    if (JOY_MEDIATOR_IS_LOCKED(mediator) || AXEL_ON_MAX_RIGHT(axel->x_pos)) return AXEL_NONE_ACTION;
     axel->x_pos += AXEL_X_SPEED;
     SPR_setHFlip(axel->sprite, TRUE);
-    return 1;
+    return AXEL_WALK_ACTION;
+}
+
+u16 axel_atack_action(joystick_mediator_struct *mediator) {
+    if (JOY_MEDIATOR_IS_LOCKED(mediator)) return AXEL_NONE_ACTION;
+    mediator->lock_frames = AXEL_ATACK_ANIMATION_FRAMES;
+    return AXEL_ATACK_ACTION;
 }
 
 void install_axel_sprite(sprite_struct *axel, const SpriteDefinition *axel_sprite) {
@@ -67,7 +77,7 @@ void install_axel_sprite(sprite_struct *axel, const SpriteDefinition *axel_sprit
     axel_joy_mediator.left_action = axel_left_action;
     axel_joy_mediator.rigth_action = axel_right_action;
     axel_joy_mediator.button_a_action = joystick_mediator_no_button_action;
-    axel_joy_mediator.button_b_action = joystick_mediator_no_button_action;
+    axel_joy_mediator.button_b_action = axel_atack_action;
     axel_joy_mediator.button_c_action = joystick_mediator_no_button_action;
     axel_joy_mediator.button_x_action = joystick_mediator_no_button_action;
     axel_joy_mediator.button_y_action = joystick_mediator_no_button_action;
@@ -83,18 +93,42 @@ void install_axel_sprite(sprite_struct *axel, const SpriteDefinition *axel_sprit
     INSTALL_SPRITE(axel_sprite, axel, AXEL_IDLE_ANIMATION);
 }
 
+void update_axel_animation(sprite_struct * axel, u16 action_taken) {
+    u16 next_animation;
+
+    switch (action_taken) {
+        case AXEL_ATACK_ACTION:
+            axel->current_animation_index = AXEL_ATACK_ANIMATION;
+            next_animation = AXEL_ATACK_ANIMATION;
+            break;
+        case AXEL_WALK_ACTION:
+            next_animation = AXEL_WALKING_ANIMATION;
+            break;
+        default:
+            next_animation = AXEL_IDLE_ANIMATION;
+            break;
+    }
+
+    if (JOY_MEDIATOR_IS_LOCKED((&axel_joy_mediator))) {
+        SPR_setAnim(axel->sprite, axel->current_animation_index);
+        return;
+    }
+    axel->current_animation_index = next_animation;
+    SPR_setAnim(axel->sprite, next_animation);
+}
+
 void update_axel_sprite(sprite_struct *axel) {
     u16 axel_action_taken = 0, joy_value = JOY_GET_CURRENT_STATE(axel_joy_mediator.joy);
 
+    // Reducing the locking time if necessary.
+    joystick_mediator_reduce_locking_time(&axel_joy_mediator);
+
     // Processing joystick state.
+    JOY_TRIGGER_B_BUTTON_ACTION(axel_joy_mediator, joy_value, axel_action_taken);
     JOY_TRIGGER_Y_AXIS_ACTION(axel_joy_mediator, joy_value, axel_action_taken);
     JOY_TRIGGER_X_AXIS_ACTION(axel_joy_mediator, joy_value, axel_action_taken);
-    JOY_TRIGGER_IDLE_ACTION(axel_joy_mediator, axel_action_taken);
     
     // Udating sprite on the screen.
-    if (axel_action_taken) {
-        axel->current_animation_index = AXEL_WALKING_ANIMATION;
-        SPR_setAnim(axel->sprite, AXEL_WALKING_ANIMATION);
-    }
+    update_axel_animation(axel, axel_action_taken);
     SPR_setPosition(axel->sprite, axel->x_pos, axel->y_pos);
 }
